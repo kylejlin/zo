a38dabae6a0efeb096aee88973c1096d38180fb0
Kyle Lin <kylejlin.us@gmail.com>
2023-06-20 18:36:02 -0700

    Split parser_tests into multiple files

---
+++ zoc/src/parser_tests/ind.rs
@@ -0,0 +1,73 @@
+use super::*;
+
+#[test]
+fn ind() {
+    let src = r#"
+(
+ind
+
+Type0
+
+"Nat"
+
+// Index types
+()
+
+// Variant constructor defs
+(
+    (() ())
+    ((0) ())
+)
+)"#;
+    let tokens = lex(src).unwrap();
+    let cst = parse(tokens).unwrap();
+    insta::assert_debug_snapshot!(&cst);
+}
+
+#[test]
+fn ind_nonliteral_universe() {
+    let src = r#"
+(
+ind
+
+1
+
+"Nat"
+
+// Index types
+()
+
+// Variant constructor defs
+(
+    (() ())
+    ((0) ())
+)
+)"#;
+    let tokens = lex(src).unwrap();
+    let err = parse(tokens).unwrap_err();
+    insta::assert_debug_snapshot!(&err);
+}
+
+#[test]
+fn ind_nonliteral_name() {
+    let src = r#"
+(
+ind
+
+Type0
+
+2
+
+// Index types
+()
+
+// Variant constructor defs
+(
+    (() ())
+    ((0) ())
+)
+)"#;
+    let tokens = lex(src).unwrap();
+    let err = parse(tokens).unwrap_err();
+    insta::assert_debug_snapshot!(&err);
+}
---
+++ zoc/src/parser_tests/mod.rs
@@ -0,0 +1,7 @@
+use crate::lexer::lex;
+use crate::parser::*;
+
+mod app;
+mod deb;
+mod ind;
+mod vcon;
