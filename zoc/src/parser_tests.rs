1eff3bef09e01dc272524f85ab656bb531310ed1
Kyle Lin <kylejlin.us@gmail.com>
2023-06-20 18:26:06 -0700

    Add more `ind` syntax tests

--- zoc/src/parser_tests.rs
+++ zoc/src/parser_tests.rs
@@ -64,3 +64,51 @@ fn ind() {
     let cst = parse(tokens).unwrap();
     insta::assert_debug_snapshot!(&cst);
 }
+
+#[test]
+fn ind_nonliteral_universe() {
+    let src = r#"
+(
+    ind
+
+    1
+
+    "Nat"
+
+    // Index types
+    ()
+
+    // Variant constructor defs
+    (
+        (() ())
+        ((0) ())
+    )
+)"#;
+    let tokens = lex(src).unwrap();
+    let cst = parse(tokens).unwrap_err();
+    insta::assert_debug_snapshot!(&cst);
+}
+
+#[test]
+fn ind_nonliteral_name() {
+    let src = r#"
+(
+    ind
+
+    Type0
+
+    2
+
+    // Index types
+    ()
+
+    // Variant constructor defs
+    (
+        (() ())
+        ((0) ())
+    )
+)"#;
+    let tokens = lex(src).unwrap();
+    let cst = parse(tokens).unwrap_err();
+    insta::assert_debug_snapshot!(&cst);
+}
