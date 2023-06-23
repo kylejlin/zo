a9d82c4fb03dd4e4384711ba6ec4f711d1443bff
Kyle Lin <kylejlin.us@gmail.com>
2023-06-23 16:46:22 -0700

    Stub TypeChecker

--- cinzo/src/typecheck.rs
+++ cinzo/src/typecheck.rs
@@ -1,14 +1,130 @@
-use crate::ast::*;
+use crate::{ast::*, eval::NormalForm, nohash_hashmap::NoHashHashMap};
+
+use std::rc::Rc;
+
+type RcHashed<T> = Rc<Hashed<T>>;
 
 #[derive(Debug, Clone)]
-pub enum TypeErr {}
+pub enum TypeError {}
 
 #[derive(Debug, Clone, Copy)]
-pub enum LazyContext<'a> {
+pub enum LazyTypeContext<'a> {
     Base(&'a [Expr]),
-    Cons(&'a [Expr], &'a LazyContext<'a>),
+    Snoc(&'a LazyTypeContext<'a>, &'a [Expr]),
 }
 
-pub fn get_type(expr: Expr, context: LazyContext) -> Result<Expr, TypeErr> {
-    todo!()
+#[derive(Debug, Clone, Copy)]
+pub enum LazySubstitutionContext<'a> {
+    Base(&'a [LazySubstitution<'a>]),
+    Cons(&'a [LazySubstitution<'a>], &'a LazySubstitutionContext<'a>),
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct LazySubstitution<'a> {
+    pub tcon_len: usize,
+    pub left: &'a Expr,
+    pub right: &'a Expr,
+}
+
+#[derive(Clone, Debug, Default)]
+pub struct TypeChecker {
+    pub type_cache: NoHashHashMap<Digest, Result<NormalForm, TypeError>>,
+}
+
+impl TypeChecker {
+    pub fn new() -> Self {
+        Self::default()
+    }
+}
+
+impl TypeChecker {
+    pub fn get_type(
+        &mut self,
+        expr: Expr,
+        tcon: LazyTypeContext,
+        scon: LazySubstitutionContext,
+    ) -> Result<Expr, TypeError> {
+        match expr {
+            Expr::Ind(e) => self.get_type_of_ind(e, tcon, scon),
+            Expr::Vcon(e) => self.get_type_of_vcon(e, tcon, scon),
+            Expr::Match(e) => self.get_type_of_match(e, tcon, scon),
+            Expr::Fun(e) => self.get_type_of_fun(e, tcon, scon),
+            Expr::App(e) => self.get_type_of_app(e, tcon, scon),
+            Expr::For(e) => self.get_type_of_for(e, tcon, scon),
+            Expr::Deb(e) => self.get_type_of_deb(e, tcon, scon),
+            Expr::Universe(e) => self.get_type_of_universe(e, tcon, scon),
+        }
+    }
+
+    fn get_type_of_ind(
+        &mut self,
+        ind: RcHashed<Ind>,
+        tcon: LazyTypeContext,
+        scon: LazySubstitutionContext,
+    ) -> Result<Expr, TypeError> {
+        todo!()
+    }
+
+    fn get_type_of_vcon(
+        &mut self,
+        vcon: RcHashed<Vcon>,
+        tcon: LazyTypeContext,
+        scon: LazySubstitutionContext,
+    ) -> Result<Expr, TypeError> {
+        todo!()
+    }
+
+    fn get_type_of_match(
+        &mut self,
+        r#match: RcHashed<Match>,
+        tcon: LazyTypeContext,
+        scon: LazySubstitutionContext,
+    ) -> Result<Expr, TypeError> {
+        todo!()
+    }
+
+    fn get_type_of_fun(
+        &mut self,
+        fun: RcHashed<Fun>,
+        tcon: LazyTypeContext,
+        scon: LazySubstitutionContext,
+    ) -> Result<Expr, TypeError> {
+        todo!()
+    }
+
+    fn get_type_of_app(
+        &mut self,
+        app: RcHashed<App>,
+        tcon: LazyTypeContext,
+        scon: LazySubstitutionContext,
+    ) -> Result<Expr, TypeError> {
+        todo!()
+    }
+
+    fn get_type_of_for(
+        &mut self,
+        r#for: RcHashed<For>,
+        tcon: LazyTypeContext,
+        scon: LazySubstitutionContext,
+    ) -> Result<Expr, TypeError> {
+        todo!()
+    }
+
+    fn get_type_of_deb(
+        &mut self,
+        deb: RcHashed<Deb>,
+        tcon: LazyTypeContext,
+        scon: LazySubstitutionContext,
+    ) -> Result<Expr, TypeError> {
+        todo!()
+    }
+
+    fn get_type_of_universe(
+        &mut self,
+        universe: RcHashed<Universe>,
+        tcon: LazyTypeContext,
+        scon: LazySubstitutionContext,
+    ) -> Result<Expr, TypeError> {
+        todo!()
+    }
 }
