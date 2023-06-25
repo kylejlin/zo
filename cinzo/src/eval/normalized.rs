ff23da363f4304deba0dc888cdb0c2a591023694
Kyle Lin <kylejlin.us@gmail.com>
2023-06-24 19:55:08 -0700

    Remove impl

--- cinzo/src/eval/normalized.rs
+++ cinzo/src/eval/normalized.rs
@@ -7,12 +7,6 @@ pub struct Normalized<T>(pub(in crate::eval) T);
 
 pub type NormalForm = Normalized<Expr>;
 
-impl From<NormalForm> for Expr {
-    fn from(nf: NormalForm) -> Self {
-        nf.0
-    }
-}
-
 impl<T> Normalized<T> {
     pub fn into_raw(self) -> T {
         self.0
