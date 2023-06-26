8f6b106a0cddd7ff56551961a3bea3698253c4de
Kyle Lin <kylejlin.us@gmail.com>
2023-06-26 14:16:30 -0700

    Extract error module

--- kozo/src/typecheck/mod.rs
+++ kozo/src/typecheck/mod.rs
@@ -12,59 +12,14 @@ use concrete_substitution::*;
 mod equality_judgment;
 use equality_judgment::*;
 
+mod error;
+use error::*;
+
 mod scon;
 use scon::*;
 
 mod tcon;
 use tcon::*;
-
-#[derive(Debug, Clone)]
-pub enum TypeError {
-    InvalidDeb {
-        deb: RcHashed<DebNode>,
-        tcon_len: usize,
-    },
-    InvalidVconIndex(RcHashed<Vcon>),
-    UnexpectedNonTypeExpression {
-        expr: Expr,
-        type_: NormalForm,
-    },
-    UniverseInconsistencyInIndDef {
-        expr: Expr,
-        level: UniverseLevel,
-        max_permitted_level: UniverseLevel,
-    },
-    WrongNumberOfIndexArguments {
-        def: VconDef,
-        expected: usize,
-        actual: usize,
-    },
-    NonInductiveMatcheeType {
-        expr: Expr,
-        type_: NormalForm,
-    },
-    WrongNumberOfMatchCases {
-        match_: RcHashed<Match>,
-        matchee_type_ind: Normalized<RcHashed<Ind>>,
-    },
-    TypeMismatch {
-        expr: Expr,
-        expected_type: NormalForm,
-        actual_type: NormalForm,
-        subbed_expected: NormalForm,
-        subbed_actual: NormalForm,
-    },
-    CalleeTypeIsNotAForExpression {
-        app: RcHashed<App>,
-        callee_type: NormalForm,
-    },
-    WrongNumberOfAppArguments {
-        app: RcHashed<App>,
-        callee_type: Normalized<RcHashed<For>>,
-        expected: usize,
-        actual: usize,
-    },
-}
 
 #[derive(Clone, Debug, Default)]
 pub struct TypeChecker {
