51f0d77fc8d39b338ae6ad403cd93bd995208b1e
Kyle Lin <kylejlin.us@gmail.com>
2023-07-06 21:55:09 -0700

    Impl fmt_universe

--- kozo/src/pretty_print/impl_for_ast.rs
+++ kozo/src/pretty_print/impl_for_ast.rs
@@ -4,55 +4,57 @@ use crate::syntax_tree::ast::*;
 
 impl Display for PrettyPrinted<'_, Expr> {
     fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
-        fmt_expr(self.0, f, 0)
+        fmt_expr(self.0, f, SoftTabCount(0))
     }
 }
 
-fn fmt_expr(expr: &Expr, f: &mut Formatter<'_>, soft_tab_count: usize) -> FmtResult {
+fn fmt_expr(expr: &Expr, f: &mut Formatter<'_>, indent: SoftTabCount) -> FmtResult {
     match expr {
-        Expr::Ind(e) => fmt_ind(e, f, soft_tab_count),
-        Expr::Vcon(e) => fmt_vcon(e, f, soft_tab_count),
-        Expr::Match(e) => fmt_match(e, f, soft_tab_count),
-        Expr::Fun(e) => fmt_fun(e, f, soft_tab_count),
-        Expr::App(e) => fmt_app(e, f, soft_tab_count),
-        Expr::For(e) => fmt_for(e, f, soft_tab_count),
-        Expr::Deb(e) => fmt_deb(e, f, soft_tab_count),
-        Expr::Universe(e) => fmt_universe(e, f, soft_tab_count),
+        Expr::Ind(e) => fmt_ind(e, f, indent),
+        Expr::Vcon(e) => fmt_vcon(e, f, indent),
+        Expr::Match(e) => fmt_match(e, f, indent),
+        Expr::Fun(e) => fmt_fun(e, f, indent),
+        Expr::App(e) => fmt_app(e, f, indent),
+        Expr::For(e) => fmt_for(e, f, indent),
+        Expr::Deb(e) => fmt_deb(e, f, indent),
+        Expr::Universe(e) => fmt_universe(e, f, indent),
     }
 }
 
-fn fmt_ind(ind: &RcSemHashed<Ind>, f: &mut Formatter<'_>, soft_tab_count: usize) -> FmtResult {
+fn fmt_ind(ind: &RcSemHashed<Ind>, f: &mut Formatter<'_>, indent: SoftTabCount) -> FmtResult {
     todo!()
 }
 
-fn fmt_vcon(vcon: &RcSemHashed<Vcon>, f: &mut Formatter<'_>, soft_tab_count: usize) -> FmtResult {
+fn fmt_vcon(vcon: &RcSemHashed<Vcon>, f: &mut Formatter<'_>, indent: SoftTabCount) -> FmtResult {
     todo!()
 }
 
-fn fmt_match(m: &RcSemHashed<Match>, f: &mut Formatter<'_>, soft_tab_count: usize) -> FmtResult {
+fn fmt_match(m: &RcSemHashed<Match>, f: &mut Formatter<'_>, indent: SoftTabCount) -> FmtResult {
     todo!()
 }
 
-fn fmt_fun(fun: &RcSemHashed<Fun>, f: &mut Formatter<'_>, soft_tab_count: usize) -> FmtResult {
+fn fmt_fun(fun: &RcSemHashed<Fun>, f: &mut Formatter<'_>, indent: SoftTabCount) -> FmtResult {
     todo!()
 }
 
-fn fmt_app(app: &RcSemHashed<App>, f: &mut Formatter<'_>, soft_tab_count: usize) -> FmtResult {
+fn fmt_app(app: &RcSemHashed<App>, f: &mut Formatter<'_>, indent: SoftTabCount) -> FmtResult {
     todo!()
 }
 
-fn fmt_for(for_: &RcSemHashed<For>, f: &mut Formatter<'_>, soft_tab_count: usize) -> FmtResult {
+fn fmt_for(for_: &RcSemHashed<For>, f: &mut Formatter<'_>, indent: SoftTabCount) -> FmtResult {
     todo!()
 }
 
-fn fmt_deb(deb: &RcSemHashed<DebNode>, f: &mut Formatter<'_>, soft_tab_count: usize) -> FmtResult {
+fn fmt_deb(deb: &RcSemHashed<DebNode>, f: &mut Formatter<'_>, indent: SoftTabCount) -> FmtResult {
     todo!()
 }
 
 fn fmt_universe(
     universe: &RcSemHashed<UniverseNode>,
     f: &mut Formatter<'_>,
-    soft_tab_count: usize,
+    indent: SoftTabCount,
 ) -> FmtResult {
-    todo!()
+    let indent_str = get_indent_str(indent);
+    let level = universe.value.level.0;
+    write!(f, "{indent_str}Type{level}")
 }
--- kozo/src/pretty_print/mod.rs
+++ kozo/src/pretty_print/mod.rs
@@ -5,4 +5,11 @@ pub const SOFT_TAB_SIZE: usize = 4;
 #[derive(Clone, Debug, Copy, PartialEq, Eq, Hash, PartialOrd, Ord)]
 pub struct PrettyPrinted<'a, T>(pub &'a T);
 
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, PartialOrd, Ord)]
+struct SoftTabCount(pub usize);
+
+fn get_indent_str(count: SoftTabCount) -> String {
+    " ".repeat(count.0 * SOFT_TAB_SIZE)
+}
+
 mod impl_for_ast;
