d4d260e2e4dd34f8d90d8ca015201f7643281b2c
Kyle Lin <kylejlin.us@gmail.com>
2023-07-20 15:09:31 -0700

    Rename `cst` params to `ost` in ost_to_ipist

--- kozo/src/syntax_tree/ost_to_ipist.rs
+++ kozo/src/syntax_tree/ost_to_ipist.rs
@@ -3,52 +3,50 @@ use crate::{
     syntax_tree::ost,
 };
 
-// TODO: Rename `cst: ...` params to `ost: ...`.
-
 impl From<ost::Expr> for ipist::Expr {
-    fn from(cst: ost::Expr) -> Self {
-        match cst {
-            ost::Expr::Ind(cst) => ipist::Ind::from(*cst).into(),
+    fn from(ost: ost::Expr) -> Self {
+        match ost {
+            ost::Expr::Ind(ost) => ipist::Ind::from(*ost).into(),
 
-            ost::Expr::Vcon(cst) => ipist::Vcon::from(*cst).into(),
+            ost::Expr::Vcon(ost) => ipist::Vcon::from(*ost).into(),
 
-            ost::Expr::Match(cst) => ipist::Match::from(*cst).into(),
+            ost::Expr::Match(ost) => ipist::Match::from(*ost).into(),
 
-            ost::Expr::Retype(cst) => ipist::Retype::from(*cst).into(),
+            ost::Expr::Retype(ost) => ipist::Retype::from(*ost).into(),
 
-            ost::Expr::Fun(cst) => ipist::Fun::from(*cst).into(),
+            ost::Expr::Fun(ost) => ipist::Fun::from(*ost).into(),
 
-            ost::Expr::App(cst) => ipist::App::from(*cst).into(),
+            ost::Expr::App(ost) => ipist::App::from(*ost).into(),
 
-            ost::Expr::For(cst) => ipist::For::from(*cst).into(),
+            ost::Expr::For(ost) => ipist::For::from(*ost).into(),
 
-            ost::Expr::Deb(cst) => ipist::Expr::Deb(rc_hashed(cst)),
+            ost::Expr::Deb(ost) => ipist::Expr::Deb(rc_hashed(ost)),
 
-            ost::Expr::Universe(cst) => ipist::Expr::Universe(rc_hashed(cst)),
+            ost::Expr::Universe(ost) => ipist::Expr::Universe(rc_hashed(ost)),
         }
     }
 }
 
 impl From<ost::Ind> for ipist::Ind {
-    fn from(cst: ost::Ind) -> Self {
+    fn from(ost: ost::Ind) -> Self {
         ipist::Ind {
-            lparen: cst.lparen,
-            type_: cst.type_,
-            name: cst.name,
-            index_types_lparen: cst.index_types_lparen,
-            index_types: (*cst.index_types).into(),
-            index_types_rparen: cst.index_types_rparen,
-            vcon_defs_lparen: cst.vcon_defs_lparen,
-            vcon_defs: (*cst.vcon_defs).into(),
-            vcon_defs_rparen: cst.vcon_defs_rparen,
-            rparen: cst.rparen,
+            lparen: ost.lparen,
+            type_: ost.type_,
+            name: ost.name,
+            index_types_lparen: ost.index_types_lparen,
+            index_types: (*ost.index_types).into(),
+            index_types_rparen: ost.index_types_rparen,
+            vcon_defs_lparen: ost.vcon_defs_lparen,
+            vcon_defs: (*ost.vcon_defs).into(),
+            vcon_defs_rparen: ost.vcon_defs_rparen,
+            rparen: ost.rparen,
         }
     }
 }
 
 impl From<ost::ZeroOrMoreExprs> for Vec<ipist::Expr> {
-    fn from(cst: ost::ZeroOrMoreExprs) -> Self {
-        match cst {
+    fn from(ost: ost::ZeroOrMoreExprs) -> Self {
+        match ost {
             ost::ZeroOrMoreExprs::Nil => vec![],
             ost::ZeroOrMoreExprs::Snoc(rdc, rac) => {
                 let mut rdc: Vec<ipist::Expr> = (*rdc).into();
@@ -60,8 +58,8 @@ impl From<ost::ZeroOrMoreExprs> for Vec<ipist::Expr> {
 }
 
 impl From<ost::ZeroOrMoreVconDefs> for Vec<ipist::VconDef> {
-    fn from(cst: ost::ZeroOrMoreVconDefs) -> Self {
-        match cst {
+    fn from(ost: ost::ZeroOrMoreVconDefs) -> Self {
+        match ost {
             ost::ZeroOrMoreVconDefs::Nil => vec![],
             ost::ZeroOrMoreVconDefs::Snoc(rdc, rac) => {
                 let mut rdc: Vec<ipist::VconDef> = (*rdc).into();
@@ -73,49 +71,49 @@ impl From<ost::ZeroOrMoreVconDefs> for Vec<ipist::VconDef> {
 }
 
 impl From<ost::VconDef> for ipist::VconDef {
-    fn from(cst: ost::VconDef) -> Self {
+    fn from(ost: ost::VconDef) -> Self {
         ipist::VconDef {
-            lparen: cst.lparen,
-            param_types_lparen: cst.param_types_lparen,
-            param_types: (*cst.param_types).into(),
-            param_types_rparen: cst.param_types_rparen,
-            index_args_lparen: cst.index_args_lparen,
-            index_args: (*cst.index_args).into(),
-            index_args_rparen: cst.index_args_rparen,
-            rparen: cst.rparen,
+            lparen: ost.lparen,
+            param_types_lparen: ost.param_types_lparen,
+            param_types: (*ost.param_types).into(),
+            param_types_rparen: ost.param_types_rparen,
+            index_args_lparen: ost.index_args_lparen,
+            index_args: (*ost.index_args).into(),
+            index_args_rparen: ost.index_args_rparen,
+            rparen: ost.rparen,
         }
     }
 }
 
 impl From<ost::Vcon> for ipist::Vcon {
-    fn from(cst: ost::Vcon) -> Self {
+    fn from(ost: ost::Vcon) -> Self {
         ipist::Vcon {
-            lparen: cst.lparen,
-            ind: rc_hashed((*cst.ind).into()),
-            vcon_index: cst.vcon_index,
-            rparen: cst.rparen,
+            lparen: ost.lparen,
+            ind: rc_hashed((*ost.ind).into()),
+            vcon_index: ost.vcon_index,
+            rparen: ost.rparen,
         }
     }
 }
 
 impl From<ost::Match> for ipist::Match {
-    fn from(cst: ost::Match) -> Self {
+    fn from(ost: ost::Match) -> Self {
         ipist::Match {
-            lparen: cst.lparen,
-            matchee: (*cst.matchee).into(),
-            econ_extension_len: cst.econ_extension_len,
-            return_type: (*cst.return_type).into(),
-            cases_lparen: cst.cases_lparen,
-            cases: (*cst.cases).into(),
-            cases_rparen: cst.cases_rparen,
-            rparen: cst.rparen,
+            lparen: ost.lparen,
+            matchee: (*ost.matchee).into(),
+            econ_extension_len: ost.econ_extension_len,
+            return_type: (*ost.return_type).into(),
+            cases_lparen: ost.cases_lparen,
+            cases: (*ost.cases).into(),
+            cases_rparen: ost.cases_rparen,
+            rparen: ost.rparen,
         }
     }
 }
 
 impl From<ost::ZeroOrMoreMatchCases> for Vec<ipist::MatchCase> {
-    fn from(cst: ost::ZeroOrMoreMatchCases) -> Self {
-        match cst {
+    fn from(ost: ost::ZeroOrMoreMatchCases) -> Self {
+        match ost {
             ost::ZeroOrMoreMatchCases::Nil => vec![],
             ost::ZeroOrMoreMatchCases::Snoc(rdc, rac) => {
                 let mut rdc: Vec<ipist::MatchCase> = (*rdc).into();
@@ -127,46 +125,46 @@ impl From<ost::ZeroOrMoreMatchCases> for Vec<ipist::MatchCase> {
 }
 
 impl From<ost::MatchCase> for ipist::MatchCase {
-    fn from(cst: ost::MatchCase) -> Self {
-        match cst {
+    fn from(ost: ost::MatchCase) -> Self {
+        match ost {
             ost::MatchCase::Dismissed(kw_index) => ipist::MatchCase::Dismissed(kw_index),
-            ost::MatchCase::Nondismissed(cst) => ipist::MatchCase::Nondismissed((*cst).into()),
+            ost::MatchCase::Nondismissed(ost) => ipist::MatchCase::Nondismissed((*ost).into()),
         }
     }
 }
 
 impl From<ost::NondismissedMatchCase> for ipist::NondismissedMatchCase {
-    fn from(cst: ost::NondismissedMatchCase) -> Self {
+    fn from(ost: ost::NondismissedMatchCase) -> Self {
         ipist::NondismissedMatchCase {
-            lparen: cst.lparen,
-            arity: cst.arity,
-            return_val: (*cst.return_val.clone()).into(),
-            rparen: cst.rparen,
+            lparen: ost.lparen,
+            arity: ost.arity,
+            return_val: (*ost.return_val.clone()).into(),
+            rparen: ost.rparen,
         }
     }
 }
 
 impl From<ost::Retype> for ipist::Retype {
-    fn from(cst: ost::Retype) -> Self {
+    fn from(ost: ost::Retype) -> Self {
         ipist::Retype {
-            lparen: cst.lparen,
-            in_term: (*cst.in_term).into(),
-            in_type: (*cst.in_type).into(),
-            out_type: (*cst.out_type).into(),
-            in_rewrites_lparen: cst.in_rewrites_lparen,
-            in_rewrites: (*cst.in_rewrites).into(),
-            in_rewrites_rparen: cst.in_rewrites_rparen,
-            out_rewrites_lparen: cst.out_rewrites_lparen,
-            out_rewrites: (*cst.out_rewrites).into(),
-            out_rewrites_rparen: cst.out_rewrites_rparen,
-            rparen: cst.rparen,
+            lparen: ost.lparen,
+            in_term: (*ost.in_term).into(),
+            in_type: (*ost.in_type).into(),
+            out_type: (*ost.out_type).into(),
+            in_rewrites_lparen: ost.in_rewrites_lparen,
+            in_rewrites: (*ost.in_rewrites).into(),
+            in_rewrites_rparen: ost.in_rewrites_rparen,
+            out_rewrites_lparen: ost.out_rewrites_lparen,
+            out_rewrites: (*ost.out_rewrites).into(),
+            out_rewrites_rparen: ost.out_rewrites_rparen,
+            rparen: ost.rparen,
         }
     }
 }
 
 impl From<ost::ZeroOrMoreRewrites> for Vec<ipist::RewriteLiteral> {
-    fn from(cst: ost::ZeroOrMoreRewrites) -> Self {
-        match cst {
+    fn from(ost: ost::ZeroOrMoreRewrites) -> Self {
+        match ost {
             ost::ZeroOrMoreRewrites::Nil => vec![],
             ost::ZeroOrMoreRewrites::Snoc(rdc, rac) => {
                 let mut rdc: Vec<_> = (*rdc).into();
@@ -178,40 +176,40 @@ impl From<ost::ZeroOrMoreRewrites> for Vec<ipist::RewriteLiteral> {
 }
 
 impl From<ost::Fun> for ipist::Fun {
-    fn from(cst: ost::Fun) -> Self {
+    fn from(ost: ost::Fun) -> Self {
         ipist::Fun {
-            lparen: cst.lparen,
-            decreasing_index: *cst.decreasing_index,
-            param_types_lparen: cst.param_types_lparen,
-            param_types: (*cst.param_types).into(),
-            param_types_rparen: cst.param_types_rparen,
-            return_type: (*cst.return_type.clone()).into(),
-            return_val: (*cst.return_val).into(),
-            rparen: cst.rparen,
+            lparen: ost.lparen,
+            decreasing_index: *ost.decreasing_index,
+            param_types_lparen: ost.param_types_lparen,
+            param_types: (*ost.param_types).into(),
+            param_types_rparen: ost.param_types_rparen,
+            return_type: (*ost.return_type.clone()).into(),
+            return_val: (*ost.return_val).into(),
+            rparen: ost.rparen,
         }
     }
 }
 
 impl From<ost::App> for ipist::App {
-    fn from(cst: ost::App) -> Self {
+    fn from(ost: ost::App) -> Self {
         ipist::App {
-            lparen: cst.lparen,
-            callee: (*cst.callee).into(),
-            args: (*cst.args).into(),
-            rparen: cst.rparen,
+            lparen: ost.lparen,
+            callee: (*ost.callee).into(),
+            args: (*ost.args).into(),
+            rparen: ost.rparen,
         }
     }
 }
 
 impl From<ost::For> for ipist::For {
-    fn from(cst: ost::For) -> Self {
+    fn from(ost: ost::For) -> Self {
         ipist::For {
-            lparen: cst.lparen,
-            param_types_lparen: cst.param_types_lparen,
-            param_types: (*cst.param_types).into(),
-            param_types_rparen: cst.param_types_rparen,
-            return_type: (*cst.return_type.clone()).into(),
-            rparen: cst.rparen,
+            lparen: ost.lparen,
+            param_types_lparen: ost.param_types_lparen,
+            param_types: (*ost.param_types).into(),
+            param_types_rparen: ost.param_types_rparen,
+            return_type: (*ost.return_type.clone()).into(),
+            rparen: ost.rparen,
         }
     }
 }
