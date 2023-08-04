6910bc216a489eec2765ddcb744abc200efdd8f9
Kyle Lin <kylejlin.us@gmail.com>
2023-08-03 23:17:29 -1000

    Extract cache_expr mod

---
+++ may/src/codegen/cache_expr.rs
@@ -0,0 +1,104 @@
+use super::*;
+
+impl MayConverter {
+    pub(crate) fn cache_expr(&mut self, node: znode::Expr) -> znode::Expr {
+        let digest = node.digest();
+        if let Some(existing) = self.znode_cache.get(digest) {
+            return existing.clone();
+        }
+
+        self.znode_cache.insert(digest.clone(), node.clone());
+        node
+    }
+
+    pub(crate) fn cache_match(&mut self, node: znode::Match) -> znode::Expr {
+        let hashed = bypass_cache_and_rc_hash(node);
+
+        if let Some(existing) = self.znode_cache.get(&hashed.digest) {
+            return existing.clone();
+        }
+
+        let digest = hashed.digest.clone();
+        let out = znode::Expr::Match(hashed);
+        self.znode_cache.insert(digest, out.clone());
+        out
+    }
+
+    pub(crate) fn cache_fun(&mut self, node: znode::Fun) -> znode::Expr {
+        let hashed = bypass_cache_and_rc_hash(node);
+
+        if let Some(existing) = self.znode_cache.get(&hashed.digest) {
+            return existing.clone();
+        }
+
+        let digest = hashed.digest.clone();
+        let out = znode::Expr::Fun(hashed);
+        self.znode_cache.insert(digest, out.clone());
+        out
+    }
+
+    pub(crate) fn cache_app(&mut self, node: znode::App) -> znode::Expr {
+        let hashed = bypass_cache_and_rc_hash(node);
+
+        if let Some(existing) = self.znode_cache.get(&hashed.digest) {
+            return existing.clone();
+        }
+
+        let digest = hashed.digest.clone();
+        let out = znode::Expr::App(hashed);
+        self.znode_cache.insert(digest, out.clone());
+        out
+    }
+
+    pub(crate) fn cache_for(&mut self, node: znode::For) -> znode::Expr {
+        let hashed = bypass_cache_and_rc_hash(node);
+
+        if let Some(existing) = self.znode_cache.get(&hashed.digest) {
+            return existing.clone();
+        }
+
+        let digest = hashed.digest.clone();
+        let out = znode::Expr::For(hashed);
+        self.znode_cache.insert(digest, out.clone());
+        out
+    }
+
+    pub(crate) fn cache_deb(&mut self, node: znode::DebNode) -> znode::Expr {
+        let hashed = bypass_cache_and_rc_hash(node);
+
+        if let Some(existing) = self.znode_cache.get(&hashed.digest) {
+            return existing.clone();
+        }
+
+        let digest = hashed.digest.clone();
+        let out = znode::Expr::Deb(hashed);
+        self.znode_cache.insert(digest, out.clone());
+        out
+    }
+
+    pub(crate) fn cache_universe(&mut self, node: znode::UniverseNode) -> znode::Expr {
+        let hashed = bypass_cache_and_rc_hash(node);
+
+        if let Some(existing) = self.znode_cache.get(&hashed.digest) {
+            return existing.clone();
+        }
+
+        let digest = hashed.digest.clone();
+        let out = znode::Expr::Universe(hashed);
+        self.znode_cache.insert(digest, out.clone());
+        out
+    }
+
+    pub(crate) fn cache_expr_vec(&mut self, node: Vec<znode::Expr>) -> RcHashedVec<znode::Expr> {
+        let hashed = bypass_cache_and_rc_hash(node);
+
+        if let Some(existing) = self.znode_vec_cache.get(&hashed.digest) {
+            return existing.clone();
+        }
+
+        let digest = hashed.digest.clone();
+        let out = hashed;
+        self.znode_vec_cache.insert(digest, out.clone());
+        out
+    }
+}
--- may/src/codegen/mod.rs
+++ may/src/codegen/mod.rs
@@ -12,6 +12,8 @@ use zoc::{
     },
 };
 
+mod cache_expr;
+
 pub mod context;
 pub use context::*;
 
@@ -282,108 +284,5 @@ impl MayConverter {
                 Ok(rdc)
             }
         }
-    }
-}
-
-impl MayConverter {
-    fn cache_expr(&mut self, node: znode::Expr) -> znode::Expr {
-        let digest = node.digest();
-        if let Some(existing) = self.znode_cache.get(digest) {
-            return existing.clone();
-        }
-
-        self.znode_cache.insert(digest.clone(), node.clone());
-        node
-    }
-
-    fn cache_match(&mut self, node: znode::Match) -> znode::Expr {
-        let hashed = bypass_cache_and_rc_hash(node);
-
-        if let Some(existing) = self.znode_cache.get(&hashed.digest) {
-            return existing.clone();
-        }
-
-        let digest = hashed.digest.clone();
-        let out = znode::Expr::Match(hashed);
-        self.znode_cache.insert(digest, out.clone());
-        out
-    }
-
-    fn cache_fun(&mut self, node: znode::Fun) -> znode::Expr {
-        let hashed = bypass_cache_and_rc_hash(node);
-
-        if let Some(existing) = self.znode_cache.get(&hashed.digest) {
-            return existing.clone();
-        }
-
-        let digest = hashed.digest.clone();
-        let out = znode::Expr::Fun(hashed);
-        self.znode_cache.insert(digest, out.clone());
-        out
-    }
-
-    fn cache_app(&mut self, node: znode::App) -> znode::Expr {
-        let hashed = bypass_cache_and_rc_hash(node);
-
-        if let Some(existing) = self.znode_cache.get(&hashed.digest) {
-            return existing.clone();
-        }
-
-        let digest = hashed.digest.clone();
-        let out = znode::Expr::App(hashed);
-        self.znode_cache.insert(digest, out.clone());
-        out
-    }
-
-    fn cache_for(&mut self, node: znode::For) -> znode::Expr {
-        let hashed = bypass_cache_and_rc_hash(node);
-
-        if let Some(existing) = self.znode_cache.get(&hashed.digest) {
-            return existing.clone();
-        }
-
-        let digest = hashed.digest.clone();
-        let out = znode::Expr::For(hashed);
-        self.znode_cache.insert(digest, out.clone());
-        out
-    }
-
-    fn cache_deb(&mut self, node: znode::DebNode) -> znode::Expr {
-        let hashed = bypass_cache_and_rc_hash(node);
-
-        if let Some(existing) = self.znode_cache.get(&hashed.digest) {
-            return existing.clone();
-        }
-
-        let digest = hashed.digest.clone();
-        let out = znode::Expr::Deb(hashed);
-        self.znode_cache.insert(digest, out.clone());
-        out
-    }
-
-    fn cache_universe(&mut self, node: znode::UniverseNode) -> znode::Expr {
-        let hashed = bypass_cache_and_rc_hash(node);
-
-        if let Some(existing) = self.znode_cache.get(&hashed.digest) {
-            return existing.clone();
-        }
-
-        let digest = hashed.digest.clone();
-        let out = znode::Expr::Universe(hashed);
-        self.znode_cache.insert(digest, out.clone());
-        out
-    }
-
-    fn cache_expr_vec(&mut self, node: Vec<znode::Expr>) -> RcHashedVec<znode::Expr> {
-        let hashed = bypass_cache_and_rc_hash(node);
-
-        if let Some(existing) = self.znode_vec_cache.get(&hashed.digest) {
-            return existing.clone();
-        }
-
-        let digest = hashed.digest.clone();
-        let out = hashed;
-        self.znode_vec_cache.insert(digest, out.clone());
-        out
     }
 }
