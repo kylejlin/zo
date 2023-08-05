e44524a3a5075a9941ac19059bfe0f33c17b1b12
Kyle Lin <kylejlin.us@gmail.com>
2023-08-04 15:32:46 -1000

    Add add_two_three test

--- may/src/codegen/tests.rs
+++ may/src/codegen/tests.rs
@@ -2,6 +2,11 @@ use super::*;
 
 use zoc::pretty_print::PrettyPrint;
 
+fn parse_or_panic(src: &str) -> mnode::Expr {
+    let tokens = crate::lexer::lex(src).unwrap();
+    crate::parser::parse(tokens).unwrap()
+}
+
 #[test]
 fn two() {
     let src = r#"
@@ -17,7 +22,29 @@ succ(succ(zero))
     insta::assert_display_snapshot!(PrettyPrint(&zo));
 }
 
-fn parse_or_panic(src: &str) -> mnode::Expr {
-    let tokens = crate::lexer::lex(src).unwrap();
-    crate::parser::parse(tokens).unwrap()
+#[test]
+fn add_two_three() {
+    let src = r#"
+ind Nat
+    case zero
+    case succ(_: Nat)
+    return Set0
+
+let _2 = succ(succ(zero))
+
+let _3 = succ(_2)
+
+fun add(-a: Nat, b: Nat): Nat
+    match a
+    case zero:
+        b
+    case succ(a_pred):
+        succ(add(a_pred, b))
+    return1 Nat
+
+add(_2, _3)
+"#;
+    let cst = parse_or_panic(src);
+    let zo = may_to_zo(&cst).unwrap();
+    insta::assert_display_snapshot!(PrettyPrint(&zo));
 }
