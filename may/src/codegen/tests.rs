437db608d6ba30a4b529b2ed95ebda7eebc684d3
Kyle Lin <kylejlin.us@gmail.com>
2023-08-06 00:28:50 -1000

    Write failing first draft of May add_commutative test

--- may/src/codegen/tests.rs
+++ may/src/codegen/tests.rs
@@ -337,3 +337,87 @@ add_n_succ_m
 
     insta::assert_display_snapshot!(PrettyPrint(&zo));
 }
+
+#[ignore]
+#[test]
+fn add_commutative() {
+    let src = r#"
+ind(T: Set0, left: T) Eq[_: T]
+    case refl: [left]
+    return Prop0
+
+ind Nat
+    case zero
+    case succ(_: Nat)
+    return Set0
+
+fun add(-a: Nat, b: Nat): Nat
+    match a
+    case zero:
+        b
+    case succ(a_pred):
+        succ(add(a_pred, b))
+    return1 Nat
+
+fun add_n_zero(-n: Nat): Eq(Nat, n)(add(n, zero))
+    match n
+    case zero:
+        refl(Nat, zero)
+    case succ(n_pred):
+        match add_n_zero(n_pred)
+        case refl:
+            refl(Nat, succ(n_pred))
+        use [in_n_pred_out_add_n_pred_zero]
+        return Eq(Nat, succ(n_pred))(succ(in_n_pred_out_add_n_pred_zero))
+    use n_capp
+    return1 Eq(Nat, n_capp)(add(n_capp, zero))
+
+fun add_n_succ_m(-n: Nat, m: Nat): Eq(Nat, succ(add(n, m)))(add(n, succ(m)))
+    match n
+    case zero:
+        refl(Nat, succ(m))
+    case succ(n_pred):
+        // Goal: Eq(Nat, succ(succ(add(n_pred, m))))(succ(add(n_pred, succ(m))))
+        match add_n_succ_m(n_pred, m)
+        case refl:
+            refl(Nat, succ(succ(add(n_pred, m))))
+        use [in_succ_add_out_add_succ]
+        return Eq(Nat, succ(succ(add(n_pred, m))))(succ(in_succ_add_out_add_succ))
+    use n_capp
+    return1 Eq(Nat, succ(add(n_capp, m)))(add(n_capp, succ(m)))
+
+// TODO: Rename `a` and `b` to `m` and `n`.
+// This is not essential.
+// However, it would be nice for the params
+// to be consistent with the params
+// in `add_n_zero` and `add_n_succ_m`.
+afun add_commutative(-a: Nat, b: Nat): Eq(Nat, add(a, b))(add(b, a))
+    match a
+    case zero:
+        match add_n_zero(b)
+        case refl:
+            refl(Nat, b)
+        use [in_b_out_add_b_zero]
+        return Eq(Nat, b)(in_b_out_add_b_zero)
+    case succ(a_pred):
+        // Goal: Eq(Nat, succ(add(a_pred, b)))(add(b, succ(a_pred)))
+        match add_n_succ_m(b, a_pred)
+        case refl:
+            // Goal: Eq(Nat, succ(add(a_pred, b)))(succ(add(b, a_pred)))
+            match add_commutative(a_pred, b)
+            case refl:
+                refl(Nat, succ(add(a_pred, b)))
+            use [in_apred_b_out_b_apred]
+            return Eq(Nat, succ(add(a_pred, b)))(succ(in_apred_b_out_b_apred))
+        use [in_succ_add_out_add_succ]
+        return Eq(Nat, succ(add(a_pred, b)))(in_succ_add_out_add_succ)
+    use a_capp
+    return1 Eq(Nat, add(a_capp, b))(add(b, a_capp))
+"#;
+    let cst = parse_or_panic(src);
+    let zo = may_to_zo(&cst).unwrap();
+
+    assert_expr_is_well_typed_under_empty_tcon(zo.clone());
+
+    insta::assert_display_snapshot!(PrettyPrint(&zo));
+}
