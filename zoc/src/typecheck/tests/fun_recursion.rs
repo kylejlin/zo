9a7314273dd7775875528aef136c39521e43b010
Kyle Lin <kylejlin.us@gmail.com>
2023-08-13 23:34:35 -0700

    Add decreasing_equal test

--- zoc/src/typecheck/tests/fun_recursion.rs
+++ zoc/src/typecheck/tests/fun_recursion.rs
@@ -138,3 +138,29 @@ fn mutual_recursion() {
     let type_ = get_type_under_empty_tcon_or_panic(&src);
     insta::assert_display_snapshot!(PrettyPrint(type_.raw()));
 }
+
+#[test]
+fn decreasing_equal() {
+    let nat_def = (
+        "<NAT>",
+        r#"
+(ind Set0 "Nat" () (
+    (() ())
+    ((0) ())
+))"#,
+    );
+    let src_defs = [nat_def];
+
+    let unsubstituted_src = r#"
+(fun 0 (<NAT>) <NAT>
+    (match 2 1 <NAT> (
+        (0 1)
+        (1 (1 2))
+    ))
+)"#;
+
+    let src = substitute_with_compounding(src_defs, unsubstituted_src);
+    let err = get_type_error_under_empty_tcon_or_panic(&src);
+    let pretty_printed_err = format!("{:#}", PrettyPrint(&err));
+    insta::assert_display_snapshot!(pretty_printed_err);
+}
