2345c61f779c68b654f2148c7da49efa3026255c
Kyle Lin <kylejlin.us@gmail.com>
2023-08-14 23:01:11 -0700

    Outline check_ind

--- zoc/src/typecheck/check_strict_positivity.rs
+++ zoc/src/typecheck/check_strict_positivity.rs
@@ -13,15 +13,15 @@ pub struct PositivityChecker<'a> {
 }
 
 #[derive(Clone, Copy, Debug)]
-enum PositivityContext<'a> {
+enum Context<'a> {
     Base(&'a [IsInd]),
-    Snoc(&'a PositivityContext<'a>, &'a [IsInd]),
+    Snoc(&'a Context<'a>, &'a [IsInd]),
 }
 
 #[derive(Clone, Copy, Debug)]
 struct IsInd(pub bool);
 
-impl PositivityContext<'static> {
+impl Context<'static> {
     pub fn empty() -> Self {
         Self::Base(&[])
     }
@@ -32,10 +32,27 @@ impl PositivityChecker<'_> {
         &mut self,
         ind: RcHashed<cst::Ind>,
     ) -> Result<(), TypeError> {
-        self.check_ind(&ind.hashee, PositivityContext::empty())
+        self.check_ind(&ind.hashee, Context::empty())
     }
 
-    fn check_ind(&mut self, ind: &cst::Ind, pcon: PositivityContext) -> Result<(), TypeError> {
+    fn check_ind(&mut self, ind: &cst::Ind, context: Context) -> Result<(), TypeError> {
+        let singleton = [IsInd(true)];
+        let extended_context = Context::Snoc(&context, &singleton);
+        self.check_vcon_defs(&ind.vcon_defs, extended_context)
+    }
+
+    fn check_vcon_defs(
+        &mut self,
+        defs: &[cst::VconDef],
+        context: Context,
+    ) -> Result<(), TypeError> {
+        for def in defs {
+            self.check_vcon_def(def, context)?;
+        }
+        Ok(())
+    }
+
+    fn check_vcon_def(&mut self, def: &cst::VconDef, context: Context) -> Result<(), TypeError> {
         // TODO
         Ok(())
     }
