9f959017e6769016d076f85bada30b7adb1fd185
Kyle Lin <kylejlin.us@gmail.com>
2023-08-18 13:39:53 -0700

    Add noninline_tree test

--- zoc/src/typecheck/tests/positivity.rs
+++ zoc/src/typecheck/tests/positivity.rs
@@ -81,6 +81,80 @@ fn inline_list_tree() {
     insta::assert_display_snapshot!(PrettyPrint(type_.raw()));
 }
 
+#[test]
+fn noninline_tree() {
+    let list_0_def = (
+        "<LIST_0>",
+        r#"
+(ind Set0 "List" () (
+    // DB index stack is
+    // 0 =>  List(T)
+    // 1 => List 
+    // 2 => T
+
+    // nil
+    (() ())
+
+    // cons
+    ((
+        2
+
+        // DB index stack is
+        // 0 => car
+        // 1 => List(T)
+        // 2 => List
+        // 3 => T
+        1
+    ) ())
+))"#,
+    );
+    let polymorphic_list_def = (
+        "<POLYMORPHIC_LIST>",
+        r#"
+(fun nonrec (Set0) Set0
+    <LIST_0>
+)"#,
+    );
+    let polymorphic_nil_def = (
+        "<POLYMORPHIC_NIL>",
+        r#"
+(fun nonrec (Set0) (<POLYMORPHIC_LIST> 0)
+    (vcon <LIST_0> 0)
+)"#,
+    );
+    let polymorphic_cons_def = (
+        "<POLYMORPHIC_CONS>",
+        r#"
+(fun nonrec (Set0) (for (0 (<POLYMORPHIC_LIST> 1)) (<POLYMORPHIC_LIST> 2))
+    (vcon <LIST_0> 1)
+)"#,
+    );
+    let tree_def = (
+        "<TREE>",
+        r#"
+(ind Set0 "Tree" () (
+    // `leaf`
+    (() ())
+
+    // `internal`
+    (((<POLYMORPHIC_LIST> 0) 1) ())
+))"#,
+    );
+    let src_defs = [
+        list_0_def,
+        polymorphic_list_def,
+        polymorphic_nil_def,
+        polymorphic_cons_def,
+        tree_def,
+    ];
+
+    let unsubstituted_src = r#"<TREE>"#;
+
+    let src = substitute_with_compounding(src_defs, unsubstituted_src);
+    let type_ = get_type_under_empty_tcon_or_panic(&src);
+    insta::assert_display_snapshot!(PrettyPrint(type_.raw()));
+}
+
 // TODO: Delete
 // #[test]
 // fn decreasing_equal() {
