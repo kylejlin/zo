aa391fef0790e33d8fde3dbc7a8cf5065e48ae53
Kyle Lin <kylejlin.us@gmail.com>
2023-08-18 14:05:09 -0700

    Re-add accidentally deleted negative test

--- zoc/src/typecheck/tests/positivity.rs
+++ zoc/src/typecheck/tests/positivity.rs
@@ -1,5 +1,73 @@
 use super::*;
 
+/// This test will currently fail
+/// because of the predicativity requirement.
+/// In that sense, it's not really a positivity test,
+/// since an error will be raised before the positivity check.
+/// However, if we support impredicativity in the future,
+/// this test will become relevant.
+/// It is a cheap safeguard against forgetting this
+/// positivity rule.
+#[test]
+fn rec_ind_in_index_arg() {
+    let false_def = (
+        "<FALSE>",
+        r#"
+(ind Set0 "False" () ())"#,
+    );
+    let src_defs = [false_def];
+
+    let unsubstituted_src = r#"
+(ind Set0 "Bad" (Set0) (
+    (() (0 <FALSE>))
+))"#;
+
+    let src = substitute_with_compounding(src_defs, unsubstituted_src);
+    let err = get_type_error_under_empty_tcon_or_panic(&src);
+    let pretty_printed_err = format!("{:#}", PrettyPrint(&err));
+    insta::assert_display_snapshot!(pretty_printed_err);
+}
+
+#[test]
+fn negative() {
+    let false_def = (
+        "<FALSE>",
+        r#"
+(ind Set0 "False" () ())"#,
+    );
+    let src_defs = [false_def];
+
+    let unsubstituted_src = r#"
+(ind Set0 "Negative" () (
+    (((for (0) <FALSE>)) ())
+))"#;
+
+    let src = substitute_with_compounding(src_defs, unsubstituted_src);
+    let err = get_type_error_under_empty_tcon_or_panic(&src);
+    let pretty_printed_err = format!("{:#}", PrettyPrint(&err));
+    insta::assert_display_snapshot!(pretty_printed_err);
+}
+
+#[test]
+fn nonstrictly_positive() {
+    let false_def = (
+        "<FALSE>",
+        r#"
+(ind Set0 "False" () ())"#,
+    );
+    let src_defs = [false_def];
+
+    let unsubstituted_src = r#"
+(ind Set0 "Negative" () (
+    (((for ((for (0) <FALSE>)) <FALSE>)) ())
+))"#;
+
+    let src = substitute_with_compounding(src_defs, unsubstituted_src);
+    let err = get_type_error_under_empty_tcon_or_panic(&src);
+    let pretty_printed_err = format!("{:#}", PrettyPrint(&err));
+    insta::assert_display_snapshot!(pretty_printed_err);
+}
+
 #[test]
 fn tree() {
     let tree_def = (
@@ -227,54 +295,6 @@ fn noninline_list_tree() {
     let src = substitute_with_compounding(src_defs, unsubstituted_src);
     let type_ = get_type_under_empty_tcon_or_panic(&src);
     insta::assert_display_snapshot!(PrettyPrint(type_.raw()));
-}
-
-#[test]
-fn nonstrictly_positive() {
-    let false_def = (
-        "<FALSE>",
-        r#"
-(ind Set0 "False" () ())"#,
-    );
-    let src_defs = [false_def];
-
-    let unsubstituted_src = r#"
-(ind Set0 "Negative" () (
-    (((for ((for (0) <FALSE>)) <FALSE>)) ())
-))"#;
-
-    let src = substitute_with_compounding(src_defs, unsubstituted_src);
-    let err = get_type_error_under_empty_tcon_or_panic(&src);
-    let pretty_printed_err = format!("{:#}", PrettyPrint(&err));
-    insta::assert_display_snapshot!(pretty_printed_err);
-}
-
-/// This test will currently fail
-/// because of the predicativity requirement.
-/// In that sense, it's not really a positivity test,
-/// since an error will be raised before the positivity check.
-/// However, if we support impredicativity in the future,
-/// this test will become relevant.
-/// It is a cheap safeguard against forgetting this
-/// positivity rule.
-#[test]
-fn rec_ind_in_index_arg() {
-    let false_def = (
-        "<FALSE>",
-        r#"
-(ind Set0 "False" () ())"#,
-    );
-    let src_defs = [false_def];
-
-    let unsubstituted_src = r#"
-(ind Set0 "Bad" (Set0) (
-    (() (0 <FALSE>))
-))"#;
-
-    let src = substitute_with_compounding(src_defs, unsubstituted_src);
-    let err = get_type_error_under_empty_tcon_or_panic(&src);
-    let pretty_printed_err = format!("{:#}", PrettyPrint(&err));
-    insta::assert_display_snapshot!(pretty_printed_err);
 }
 
 // TODO: Add more tests.
