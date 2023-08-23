c7e6b273a382c023cece1739a939dceddee184bf
Kyle Lin <kylejlin.us@gmail.com>
2023-08-23 01:23:34 -0700

    Remove unused methods

--- zoc/src/check_erasability.rs
+++ zoc/src/check_erasability.rs
@@ -45,66 +45,12 @@ where
 impl ErasabilityChecker {
     fn check(&mut self, expr: Expr, tcon: LazyTypeContext) -> Result<(), ErasabilityError> {
         match expr {
-            Expr::Ind(e) => self.check_ind(e, tcon),
-            Expr::Vcon(e) => self.check_vcon(e, tcon),
             Expr::Match(e) => self.check_match(e, tcon),
             Expr::Fun(e) => self.check_fun(e, tcon),
             Expr::App(e) => self.check_app(e, tcon),
             Expr::For(e) => self.check_for(e, tcon),
-            Expr::Deb(_) | Expr::Universe(_) => Ok(()),
+            Expr::Ind(_) | Expr::Vcon(_) | Expr::Deb(_) | Expr::Universe(_) => Ok(()),
         }
-    }
-
-    fn check_ind(
-        &mut self,
-        ind: RcHashed<Ind>,
-        tcon_g0: LazyTypeContext,
-    ) -> Result<(), ErasabilityError> {
-        self.check_dependent_exprs(&ind.hashee.index_types.hashee, tcon_g0)?;
-
-        let ind_type_g0 = self
-            .typechecker
-            .get_type_of_ind(ind.clone(), tcon_g0)
-            .expect_well_typed();
-        let ind_singleton = Normalized::<[Expr; 1]>::new(ind_type_g0);
-        let tcon_with_ind_type_g1 =
-            LazyTypeContext::Snoc(&tcon_g0, ind_singleton.as_ref().convert_ref());
-
-        self.check_vcon_defs(&ind.hashee.vcon_defs.hashee, tcon_with_ind_type_g1)?;
-
-        Ok(())
-    }
-
-    fn check_vcon_defs(
-        &mut self,
-        defs: &[VconDef],
-        tcon: LazyTypeContext,
-    ) -> Result<(), ErasabilityError> {
-        for def in defs {
-            self.check_vcon_def(def, tcon)?;
-        }
-        Ok(())
-    }
-
-    fn check_vcon_def(
-        &mut self,
-        def: &VconDef,
-        tcon_g0: LazyTypeContext,
-    ) -> Result<(), ErasabilityError> {
-        let param_types = self.check_dependent_exprs(&def.param_types.hashee, tcon_g0)?;
-        let tcon_with_param_types_g1 = LazyTypeContext::Snoc(&tcon_g0, param_types.to_derefed());
-
-        self.check_independent_exprs(&def.index_args.hashee, tcon_with_param_types_g1)?;
-
-        Ok(())
-    }
-
-    fn check_vcon(
-        &mut self,
-        vcon: RcHashed<Vcon>,
-        tcon: LazyTypeContext,
-    ) -> Result<(), ErasabilityError> {
-        self.check_ind(vcon.hashee.ind.clone(), tcon)
     }
 
     fn check_match(
@@ -144,6 +90,8 @@ impl ErasabilityChecker {
         exprs: &[Expr],
         tcon: LazyTypeContext,
     ) -> Result<Normalized<Vec<minimal_ast::Expr>>, ErasabilityError> {
+        // TODO: Fix
+
         let mut out: Normalized<Vec<minimal_ast::Expr>> = Normalized::with_capacity(exprs.len());
         let mut normalized_visited_exprs: Normalized<Vec<minimal_ast::Expr>> =
             Normalized::with_capacity(exprs.len());
