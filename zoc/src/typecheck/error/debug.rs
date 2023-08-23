6a74766556e003f8e541eb66ba98292cbbd78c97
Kyle Lin <kylejlin.us@gmail.com>
2023-08-22 17:54:35 -0700

    Split typecheck::error into multiple files

---
+++ zoc/src/typecheck/error/debug.rs
@@ -0,0 +1,17 @@
+use super::*;
+
+use std::fmt::{Debug, Result as FmtResult};
+
+impl Debug for TypeError<UnitAuxData> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> FmtResult {
+        let pretty = self.pretty_printed();
+        write!(f, "{pretty:#}")
+    }
+}
+
+impl Debug for TypeError<SpanAuxData> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> FmtResult {
+        let pretty = self.pretty_printed();
+        write!(f, "{pretty:#}")
+    }
+}
