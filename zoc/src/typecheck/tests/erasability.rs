64277a3be26c95e94bc61709456fd05c53b9d6b2
Kyle Lin <kylejlin.us@gmail.com>
2023-08-22 18:06:13 -0700

    Add ng_1_variant_erasable_with_nonerasable_vcon_def_param_types_to_nonerasable

--- zoc/src/typecheck/tests/erasability.rs
+++ zoc/src/typecheck/tests/erasability.rs
@@ -42,3 +42,37 @@ fn ng_2_variant_erasable_to_nonerasable() {
     let pretty_printed_err = format!("{:#}", PrettyPrint(&err));
     insta::assert_display_snapshot!(pretty_printed_err);
 }
+
+#[test]
+fn ng_1_variant_erasable_with_nonerasable_vcon_def_param_types_to_nonerasable() {
+    let bool_set_def = (
+        "<BOOL_SET>",
+        r#"
+(ind Set0 "Bool" () (
+    (() ())
+    (() ())
+))"#,
+    );
+    let true_set_def = ("<TRUE_SET>", r#"(vcon <BOOL_SET> 0)"#);
+    let false_set_def = ("<FALSE_SET>", r#"(vcon <BOOL_SET> 1)"#);
+    let foo_def = (
+        "<FOO>",
+        r#"
+(ind Prop0 "Foo" () (
+    ((<BOOL_SET>) ())
+))"#,
+    );
+    let src_defs = [bool_set_def, true_set_def, false_set_def, foo_def];
+
+    let unsubstituted_src = r#"
+(fun nonrec (<FOO>) <BOOL_SET>
+    (match 1 1 <BOOL_SET> (
+        (1 0)
+    ))
+)"#;
+
+    let src = substitute_with_compounding(src_defs, unsubstituted_src);
+    let err = get_type_error_under_empty_tcon_or_panic(&src);
+    let pretty_printed_err = format!("{:#}", PrettyPrint(&err));
+    insta::assert_display_snapshot!(pretty_printed_err);
+}
