82a319dbee9039b8767797bb5afa88fed2725980
Kyle Lin <kylejlin.us@gmail.com>
2023-10-08 00:38:55 +0900

    Add parses_valid_input_correctly test

--- june/src/parser/mod.rs
+++ june/src/parser/mod.rs
@@ -4,3 +4,17 @@ mod generated_parser;
 pub mod cst {
     pub use super::generated_parser::*;
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use crate::lexer::lex;
+
+    #[test]
+    fn parses_valid_input_correctly() {
+        let src = include_str!("./parser_test_sample_code.jn");
+        let tokens = lex(src).unwrap();
+        parse(tokens).unwrap();
+    }
+}
